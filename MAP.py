63c8489a413d9b9acb1a0c01f902ba1137859f76
Sklud1456 <63137075+Sklud1456@users.noreply.github.com>
2022-09-29 10:55:15 +0800

    Add files via upload

--- MAP.py
+++ MAP.py
@@ -3,75 +3,73 @@ from queue import Queue
 import random
 
 Direction = {
-	"RIGHT":0, "UP":1, "LEFT":2, "DOWN":3, "NONE":-1
+    "RIGHT": 0, "UP": 1, "LEFT": 2, "DOWN": 3, "NONE": -1
 }
 
 MoveTO = []
-MoveTO.append(np.array([1, 0]))		# RIGHT
-MoveTO.append(np.array([0, -1]))	# UP
-MoveTO.append(np.array([-1, 0]))	# LEFT
-MoveTO.append(np.array([0, 1]))		# DOWN
+MoveTO.append(np.array([1, 0]))  # RIGHT
+MoveTO.append(np.array([0, -1]))  # UP
+MoveTO.append(np.array([-1, 0]))  # LEFT
+MoveTO.append(np.array([0, 1]))  # DOWN
 
-MoveTO.append(np.array([1, -1]))	
-MoveTO.append(np.array([-1, -1]))	
-MoveTO.append(np.array([-1, 1]))	
-MoveTO.append(np.array([1, 1]))		
-
-
+MoveTO.append(np.array([1, -1]))
+MoveTO.append(np.array([-1, -1]))
+MoveTO.append(np.array([-1, 1]))
+MoveTO.append(np.array([1, 1]))
 
 
 # 输入：一条线段的两个端点
 # 输出：整点集合
 def Init_Exit(P1, P2):
-	exit = list()
-	
-	if P1[0]==P2[0]:
-		x = P1[0]
-		for y in range(P1[1], P2[1]+1):
-			exit.append((x, y))
-	elif P1[1]==P2[1]:
-		y = P1[1]
-		for x in range(P1[0], P2[0]+1):
-			exit.append((x, y))
-	# 斜线 
-	else :
-		pass
+    exit = list()
 
-	return exit
+    if P1[0] == P2[0]:
+        x = P1[0]
+        for y in range(P1[1], P2[1] + 1):
+            exit.append((x, y))
+    elif P1[1] == P2[1]:
+        y = P1[1]
+        for x in range(P1[0], P2[0] + 1):
+            exit.append((x, y))
+    # 斜线
+    else:
+        pass
+
+    return exit
+
 
 # 两点坐标围成的矩形区域
 def Init_Barrier(A, B):
-	if A[0]>B[0]:
-		A, B = B, A
+    if A[0] > B[0]:
+        A, B = B, A
 
-	x1, y1 = A[0], A[1]
-	x2, y2 = B[0], B[1]
+    x1, y1 = A[0], A[1]
+    x2, y2 = B[0], B[1]
 
-	if y1<y2:
-		return ((x1, y1), (x2, y2))
-	else:
-		return ((x1, y2), (x2, y1))
+    if y1 < y2:
+        return ((x1, y1), (x2, y2))
+    else:
+        return ((x1, y2), (x2, y1))
 
 
 def Init_Barrier1(A, B, C):
-	if A[0]>B[0]:
-		A, B = B, A
+    if A[0] > B[0]:
+        A, B = B, A
 
-	x1, y1 = int(A[0]/3), 75-int(A[1]/3)
-	x2, y2 = int(B[0]/3), 75-int(B[1]/3)
+    x1, y1 = int(A[0] / 3), 75 - int(A[1] / 3)
+    x2, y2 = int(B[0] / 3), 75 - int(B[1] / 3)
 
-
-
-	if y1<y2:
-		return ((x1, y1), (x2, y2), C)
-	else:
-		return ((x1, y2), (x2, y1), C)
+    if y1 < y2:
+        return ((x1, y1), (x2, y2), C)
+    else:
+        return ((x1, y2), (x2, y1), C)
 
 
 # 
 # 外墙宽度 
 # Size
 Outer_Size = 1
+
 
 # 障碍 	-1
 # LEFT 	0
@@ -81,170 +79,172 @@ Outer_Size = 1
 
 
 class Map:
-	def __init__(self, L, W, E, B):
-		self.Length = L
-		self.Width = W
-		self.Exit = E
-		self.Barrier = B
-		self.barrier_list = []
+    def __init__(self, L, W, E, B):
+        self.Length = L
+        self.Width = W
+        self.Exit = E
+        self.Barrier = B
+        self.barrier_list = []
 
-		# 0~L+1
-		# 0~W+1
-		# 势能
-		# 出口为 1
-		# 障碍为 inf
-		self.space = np.zeros((self.Length+Outer_Size*2, self.Width+Outer_Size*2))
-		for j in range(0, self.Width+Outer_Size*2):
-			self.space[0][j] = self.space[L+1][j] = float("inf")
-			self.barrier_list.append((0, j))
-			self.barrier_list.append((L+1, j))
+        # 0~L+1
+        # 0~W+1
+        # 势能
+        # 出口为 1
+        # 障碍为 inf
+        self.space = np.zeros((self.Length + Outer_Size * 2, self.Width + Outer_Size * 2))
+        for j in range(0, self.Width + Outer_Size * 2):
+            self.space[0][j] = self.space[L + 1][j] = float("inf")
+            self.barrier_list.append((0, j))
+            self.barrier_list.append((L + 1, j))
 
-		for i in range(0, self.Length+Outer_Size*2):
-			self.space[i][0] = self.space[i][W+1] = float("inf")
-			self.barrier_list.append((i, 0))
-			self.barrier_list.append((i, W+1))
+        for i in range(0, self.Length + Outer_Size * 2):
+            self.space[i][0] = self.space[i][W + 1] = float("inf")
+            self.barrier_list.append((i, 0))
+            self.barrier_list.append((i, W + 1))
 
-		for (A, B, C) in self.Barrier:
-			for i in range(A[0], B[0]+1):
-				for j in range(A[1], B[1]+1):
-					self.space[i][j] = float("inf")
-					self.barrier_list.append((i, j))
+        for (A, B, C) in self.Barrier:
+            for i in range(A[0], B[0] + 1):
+                for j in range(A[1], B[1] + 1):
+                    self.space[i][j] = float("inf")
+                    self.barrier_list.append((i, j))
 
-		# 出口
-		for (ex, ey) in self.Exit:
-			self.space[ex][ey] = 1
-			if ex==self.Length:
-				self.space[ex+1][ey] = 1
-			if ey==self.Width:
-				self.space[ex][ey+1] = 1
-			# #print("%d %d"%(ex, ey))
-			if (ex, ey) in self.barrier_list:
-				self.barrier_list.remove((ex, ey))
+        # 出口
+        for (ex, ey) in self.Exit:
+            self.space[ex][ey] = 1
+            if ex == self.Length:
+                self.space[ex + 1][ey] = 1
+            if ey == self.Width:
+                self.space[ex][ey + 1] = 1
+            # #print("%d %d"%(ex, ey))
+            if (ex, ey) in self.barrier_list:
+                self.barrier_list.remove((ex, ey))
 
-		# #print(self.barrier_list)
+        # #print(self.barrier_list)
 
-		# #print(type(self.space))
-		# 
-		# 显示全部
-		# #print(self.space)
-		
-		self.Init_Potential()
-		# self.print(self.space)
+        # #print(type(self.space))
+        #
+        # 显示全部
+        # #print(self.space)
 
-	def print(self, mat):
-		for line in mat:
-			for v in line:
-				print(v, end=' ')
-			print("")
-	
-	def Check_Valid(self, x, y):
-		# pass
-		x, y = int(x), int(y)
-		# 边缘
-		if x>self.Length+1 or x<0 or y>self.Width+1 or y<0:
-			return False
-		# 障碍
-		if self.space[x][y]==float("inf"):
-			return False
-		else:
-			return True
-	def checkSavefy(self, pos):
-		x, y = int(pos[0]), int(pos[1])
-		if x==self.Length+1:
-			x -= 1
-		elif x==-1:
-			x += 1
-		if y==self.Width+1:
-			y -= 1
-		elif y==-1:
-			y -= 0
+        self.Init_Potential()
 
-		if (x, y) in self.Exit:
-			return True
-		else:
-			return False
+    # self.print(self.space)
 
-	def getDeltaP(self, P1, P2):
-		x1, y1 = int(P1[0]), int(P1[1])
-		x2, y2 = int(P2[0]), int(P2[1])
-		return self.space[x1][y1] - self.space[x2][y2]
+    def print(self, mat):
+        for line in mat:
+            for v in line:
+                print(v, end=' ')
+            print("")
 
-	def Init_Potential(self):
-		minDis = np.zeros((self.Length+Outer_Size*2, self.Width+Outer_Size*2))
-		for i in range(self.Length+Outer_Size*2):
-			for j in range(self.Width+Outer_Size*2):
-				minDis[i][j] = float("inf")
+    def Check_Valid(self, x, y):
+        # pass
+        x, y = int(x), int(y)
+        # 边缘
+        if x > self.Length + 1 or x < 0 or y > self.Width + 1 or y < 0:
+            return False
+        # 障碍
+        if self.space[x][y] == float("inf"):
+            return False
+        else:
+            return True
 
-		# #print(minDis)
-		for (sx, sy) in self.Exit:
-			#print(sx, sy)
-			tmp = self.BFS(sx, sy)
-			# self.#print(tmp)
-			#print("----")
-			for i in range(self.Length+Outer_Size*2):
-				for j in range(self.Width+Outer_Size*2):
-					minDis[i][j] = min(minDis[i][j], tmp[i][j])
+    def checkSavefy(self, pos):
+        x, y = int(pos[0]), int(pos[1])
+        if x == self.Length + 1:
+            x -= 1
+        elif x == -1:
+            x += 1
+        if y == self.Width + 1:
+            y -= 1
+        elif y == -1:
+            y -= 0
 
-		self.space = minDis
-		print(minDis.shape)
-		# return minDis
-		# #print(minDis)
+        if (x, y) in self.Exit:
+            return True
+        else:
+            return False
 
-	# 确立势能地图
-	def BFS(self, x, y):
-		if not self.Check_Valid(x, y):
-			return
+    def getDeltaP(self, P1, P2):
+        x1, y1 = int(P1[0]), int(P1[1])
+        x2, y2 = int(P2[0]), int(P2[1])
+        return self.space[x1][y1] - self.space[x2][y2]
 
+    def Init_Potential(self):
+        minDis = np.zeros((self.Length + Outer_Size * 2, self.Width + Outer_Size * 2))
+        for i in range(self.Length + Outer_Size * 2):
+            for j in range(self.Width + Outer_Size * 2):
+                minDis[i][j] = float("inf")
 
-		tmpDis = np.zeros((self.Length+Outer_Size*2, self.Width+Outer_Size*2))
-		for i in range(self.Length+Outer_Size*2):
-			for j in range(self.Width+Outer_Size*2):
-				tmpDis[i][j] = self.space[i][j]
+        # #print(minDis)
+        for (sx, sy) in self.Exit:
+            # print(sx, sy)
+            tmp = self.BFS(sx, sy)
+            # self.#print(tmp)
+            # print("----")
+            for i in range(self.Length + Outer_Size * 2):
+                for j in range(self.Width + Outer_Size * 2):
+                    minDis[i][j] = min(minDis[i][j], tmp[i][j])
 
-		queue = Queue()
-		queue.put((x, y))
-		tmpDis[x][y] = 1
-		while not queue.empty():
-			(x, y) = queue.get()
-			dis = tmpDis[x][y]
-			# if dis>0:
-			# 	continue
+        self.space = minDis
+        print(minDis.shape)
 
-			for i in range(8):
-				move = MoveTO[i]
-				(nx, ny) = (x, y) + move
-				if self.Check_Valid(nx, ny) and tmpDis[nx][ny]==0:
-					queue.put((nx, ny))
-					tmpDis[nx][ny] = dis + (1.0 if i<4 else 1.4)
+    # return minDis
+    # #print(minDis)
 
-		return tmpDis
+    # 确立势能地图
+    def BFS(self, x, y):
+        if not self.Check_Valid(x, y):
+            return
 
+        tmpDis = np.zeros((self.Length + Outer_Size * 2, self.Width + Outer_Size * 2))
+        for i in range(self.Length + Outer_Size * 2):
+            for j in range(self.Width + Outer_Size * 2):
+                tmpDis[i][j] = self.space[i][j]
 
-	def Random_Valid_Point(self):
-		x = random.uniform(1, self.Length+2)
-		y = random.uniform(1, self.Width+2)
-		while not myMap.Check_Valid(x, y):
-			x = random.uniform(1, self.Length+2)
-			y = random.uniform(1, self.Width+2)
+        queue = Queue()
+        queue.put((x, y))
+        tmpDis[x][y] = 1
+        while not queue.empty():
+            (x, y) = queue.get()
+            dis = tmpDis[x][y]
+            # if dis>0:
+            # 	continue
 
-		return x, y
+            for i in range(8):
+                move = MoveTO[i]
+                (nx, ny) = (x, y) + move
+                if self.Check_Valid(nx, ny) and tmpDis[nx][ny] == 0:
+                    queue.put((nx, ny))
+                    tmpDis[nx][ny] = dis + (1.0 if i < 4 else 1.4)
+
+        return tmpDis
+
+    def Random_Valid_Point(self):
+        x = random.uniform(1, self.Length + 2)
+        y = random.uniform(1, self.Width + 2)
+        while not myMap.Check_Valid(x, y):
+            x = random.uniform(1, self.Length + 2)
+            y = random.uniform(1, self.Width + 2)
+
+        return x, y
+
 
 def Init_Map():
-	# 房间长宽
-	Length = 60
-	Width = 10
+    # 房间长宽
+    Length = 60
+    Width = 10
 
-	# 出口 
-	# 点集
-	Exit = Init_Exit(P1=(60, 2), P2=(60, 4))
-	Exit.extend(Init_Exit(P1=(0, 8), P2=(0, 9)))
+    # 出口
+    # 点集
+    Exit = Init_Exit(P1=(60, 2), P2=(60, 4))
+    Exit.extend(Init_Exit(P1=(0, 8), P2=(0, 9)))
 
-	# 障碍 矩形区域
-	Barrier = list()
-	Barrier.append(Init_Barrier(A=(3, 5), B=(60, 7)))
-	Barrier.append(Init_Barrier(A=(10, 6), B=(15, 8)))
+    # 障碍 矩形区域
+    Barrier = list()
+    Barrier.append(Init_Barrier(A=(3, 5), B=(60, 7)))
+    Barrier.append(Init_Barrier(A=(10, 6), B=(15, 8)))
 
-	return Map(L=Length, W=Width, E=Exit, B=Barrier)
+    return Map(L=Length, W=Width, E=Exit, B=Barrier)
 
 
 # # 房间长宽
@@ -266,8 +266,7 @@ def Init_Map():
 # myMap = Map(L=Length, W=Width, E=Exit, B=Barrier)
 
 
-
-# # 出口 
+# # 出口
 # # 点集
 # Exit = Init_Exit(P1=(100, 28), P2=(100, 32))
 # Exit.extend(Init_Exit(P1=(0, 28), P2=(0, 32)))
@@ -287,7 +286,9 @@ Width = 75
 
 # 出口 
 # 点集
+Exits = list()
 Exit = Init_Exit(P1=(100, 27), P2=(100, 29))
+
 # Exit.extend(Init_Exit(P1=(0, 25), P2=(0, 35)))
 
 # 甲乙丙丁戊危险划分
@@ -348,13 +349,5 @@ Barrier.append(Init_Barrier1(A=(234, 22), B=(248, 34), C=1))
 # 三废处理站
 Barrier.append(Init_Barrier1(A=(260, 22), B=(278, 57), C=2))
 
-
-
-
-
-
-
-#第一种情况
+# 第一种情况
 myMap = Map(L=Length, W=Width, E=Exit, B=Barrier)
-
-
--- main.py
+++ main.py
@@ -9,120 +9,130 @@ import random
 from MAP import myMap
 from people import People
 
+
 class GUI:
-	# GUI
-	# 图像比例
-	Pic_Ratio = 10
-	def __init__(self):
-		self.root = tk.Tk()
-		self.root.title("疏散模拟")
-		self.root.geometry("1100x900")
-		self.root.resizable(width=True, height=True)
+    # GUI
+    # 图像比例
+    Pic_Ratio = 10
 
-		width = myMap.Length * GUI.Pic_Ratio
-		height = myMap.Width * GUI.Pic_Ratio
-		self.canvas = tk.Canvas(self.root, width=width, height=height, bg="grey")
-		self.canvas.pack()
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("疏散模拟")
+        self.root.geometry("1100x900")
+        self.root.resizable(width=True, height=True)
 
-		self.label_time = tk.Label(self.root, text="Time = 0.00s", font='Arial -37 bold')
-		self.label_evac = tk.Label(self.root, text="Evacution People: 0", font='Arial -37 bold')
-		self.label_time.pack()
-		self.label_evac.pack()
+        width = myMap.Length * GUI.Pic_Ratio
+        height = myMap.Width * GUI.Pic_Ratio
+        self.canvas = tk.Canvas(self.root, width=width, height=height, bg="grey")
+        self.canvas.pack()
 
-		self.setBarrier()
-		self.setExit()
+        self.label_text = tk.Label(self.root, text="疏散人数:", font='Arial -37 bold')
+        self.label_time = tk.Label(self.root, text="Time = 0.00s", font='Arial -37 bold')
+        self.label_evac = tk.Label(self.root, text="Evacution People: 0", font='Arial -37 bold')
+        self.entry_peoplenumber = tk.Entry(self.root, font=(None, 35), width=6)
 
-	
-	# 障碍
-	def setBarrier(self):
-		for (A, B, C) in myMap.Barrier:
-			x1, y1, x2, y2 = A[0], A[1], B[0], B[1]
-			[x1, y1, x2, y2] = map(lambda x:x*GUI.Pic_Ratio, [x1, y1, x2, y2])
-			str = ""
-			text=""
-			if C == 1:
-				str = "#FF0000"  # 红色
-				text = "甲类"
-			elif C == 2:
-				str = "#B22222"  # 砖红色
-				text = "乙类"
-			elif C == 3:
-				str = "#FF8C00"  # 橙色
-				text = "丙类"
-			elif C == 4:
-				str = "#FFFF00"  # 黄色
-				text = "丁类"
-			elif C == 5:
-				str = "#0000FF"  # 蓝色
-				text = "戊类"
-			self.canvas.create_rectangle(x1, y1, x2, y2, fill=str, outline=str)
-			self.canvas.create_text(int((x1+x2)/2),int((y1+y2)/2),text=text,fill ='#7CCD7C',anchor = "center",font =('微软雅黑',15,'bold'))
+        self.button_start = tk.Button(self.root, text="开始", width=5, font='Arial -37 bold',
+                                      command=lambda: Cellular_Automata(int(self.entry_peoplenumber.get())))
+        self.label_time.place(x=40, y=760)
+        self.label_evac.place(x=40, y=810)
+        self.button_start.place(x=925, y=760)
+        self.label_text.place(x=580, y=770)
+        self.entry_peoplenumber.place(x=760, y=770)
 
-	# 出口
-	def setExit(self):
-		for (x, y) in myMap.Exit:
-			sx, sy = x-1, y-1
-			ex, ey = x+1, y+1
-			[sx, sy, ex, ey] = map(lambda x:x*GUI.Pic_Ratio, [sx, sy, ex, ey])
-			self.canvas.create_rectangle(sx, sy, ex, ey, fill="green", outline="green")
+        self.setBarrier()
+        self.setExit()
 
+    # 障碍
+    def setBarrier(self):
+        for (A, B, C) in myMap.Barrier:
+            x1, y1, x2, y2 = A[0], A[1], B[0], B[1]
+            [x1, y1, x2, y2] = map(lambda x: x * GUI.Pic_Ratio, [x1, y1, x2, y2])
+            str = ""
+            text = ""
+            if C == 1:
+                str = "#FF0000"  # 红色
+                text = "甲类"
+            elif C == 2:
+                str = "#B22222"  # 砖红色
+                text = "乙类"
+            elif C == 3:
+                str = "#FF8C00"  # 橙色
+                text = "丙类"
+            elif C == 4:
+                str = "#FFFF00"  # 黄色
+                text = "丁类"
+            elif C == 5:
+                str = "#0000FF"  # 蓝色
+                text = "戊类"
+            self.canvas.create_rectangle(x1, y1, x2, y2, fill=str, outline=str)
+            self.canvas.create_text(int((x1 + x2) / 2), int((y1 + y2) / 2), text=text, fill='#7CCD7C', anchor="center",
+                                    font=('微软雅黑', 15, 'bold'))
 
-	def Update_People(self, People_List):
-		for p in People_List:
-			# print(p.id)
-			self.canvas.delete(p.name())
+    # 出口
+    def setExit(self):
+        for (x, y) in myMap.Exit:
+            sx, sy = x - 1, y - 1
+            ex, ey = x + 1, y + 1
+            [sx, sy, ex, ey] = map(lambda x: x * GUI.Pic_Ratio, [sx, sy, ex, ey])
+            self.canvas.create_rectangle(sx, sy, ex, ey, fill="green", outline="green")
 
-		self.Show_People(People_List)
+    def Update_People(self, People_List):
+        for p in People_List:
+            # print(p.id)
+            self.canvas.delete(p.name())
 
-	def Show_People(self, People_List):
-		for p in People_List:
-			if p.savety:
-				continue
-			ox, oy = p.pos[0], p.pos[1]
-			x1, y1 = ox-0.2, oy-0.2
-			x2, y2 = ox+0.2, oy+0.2
-			[x1, y1, x2, y2] = map(lambda x:x*GUI.Pic_Ratio, [x1, y1, x2, y2])
-			self.canvas.create_oval(x1, y1, x2, y2, fill="black", outline="black", tag=p.name())
-			
+        self.Show_People(People_List)
+
+    def Show_People(self, People_List):
+        for p in People_List:
+            if p.savety:
+                continue
+            ox, oy = p.pos[0], p.pos[1]
+            x1, y1 = ox - 0.2, oy - 0.2
+            x2, y2 = ox + 0.2, oy + 0.2
+            [x1, y1, x2, y2] = map(lambda x: x * GUI.Pic_Ratio, [x1, y1, x2, y2])
+            self.canvas.create_oval(x1, y1, x2, y2, fill="black", outline="black", tag=p.name())
+
 
 def Cellular_Automata(Total_People):
-	
-	UI = GUI()
+    # UI = GUI()
 
-	# Total_People = 200
-	P = People(Total_People, myMap)
+    # Total_People = 200
+    P = People(Total_People, myMap)
 
-	UI.Show_People(P.list)
+    UI.Show_People(P.list)
 
+    Time_Start = time.time()
+    Eva_Number = 0
+    while Eva_Number < Total_People:
+        Eva_Number = P.run()
 
-	Time_Start = time.time()
-	Eva_Number = 0
-	while Eva_Number<Total_People:
-		Eva_Number = P.run()
+        UI.Update_People(P.list)
 
-		UI.Update_People(P.list)	
+        # time.sleep(random.uniform(0.05, 0.15))
+        time.sleep(0.01)
+        UI.canvas.update()
+        UI.root.update()
 
-		# time.sleep(random.uniform(0.05, 0.15))
-		time.sleep(0.01)
-		UI.canvas.update()
-		UI.root.update()
+        Time_Pass = time.time() - Time_Start
+        UI.label_time['text'] = "Time = " + "%.2f" % Time_Pass + "s"
+        UI.label_evac['text'] = "Evacution People: " + str(Eva_Number)
+    # print("%.2fs" % (Time_Pass) + " 已疏散人数:" +str(Eva_Number))
 
-		Time_Pass = time.time()-Time_Start
-		UI.label_time['text'] = "Time = "+ "%.2f" % Time_Pass + "s"
-		UI.label_evac['text'] = "Evacution People: " + str(Eva_Number)
-		# print("%.2fs" % (Time_Pass) + " 已疏散人数:" +str(Eva_Number))
+    Time_Pass = time.time() - Time_Start
+    UI.label_time['text'] = "Time = " + "%.2f" % Time_Pass + "s"
+    UI.label_evac['text'] = "Evacution People: " + str(Eva_Number)
 
-	Time_Pass = time.time()-Time_Start
-	UI.label_time['text'] = "Time = "+  "%.2f" % Time_Pass + "s"
-	UI.label_evac['text'] = "Evacution People: " + str(Eva_Number)
-
-	# 热力图
-	sns.heatmap(P.thmap.T, cmap='Reds')
-	plt.axis('equal')
-	plt.show()
+    # 热力图
+    sns.heatmap(P.thmap.T, cmap='Reds')
+    plt.axis('equal')
+    plt.show()
 
 
-	UI.root.mainloop()
+# UI.root.mainloop()
 
-Cellular_Automata(Total_People=1000)
 
+UI = GUI()
+
+UI.root.mainloop()
+# Cellular_Automata(Total_People=1000)
